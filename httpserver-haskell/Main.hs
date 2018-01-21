f4d00b7a1d5361485400276c4d498046574e6587
Mikael Johansson <mikejoh@mij-praqma.local>
2018-01-21 21:43:33 +0100

    Added Haskell example using the Spock web framework

---
+++ httpserver-haskell/Main.hs
@@ -0,0 +1,24 @@
+{-# LANGUAGE OverloadedStrings #-}
+module Main where
+
+import Web.Spock
+import Web.Spock.Config
+
+import Control.Monad.Trans
+import Data.Monoid
+import Data.IORef
+import qualified Data.Text as T
+
+data MySession = EmptySession
+data MyAppState = DummyAppState (IORef Int)
+
+main :: IO ()
+main =
+    do ref <- newIORef 0
+       spockCfg <- defaultSpockCfg EmptySession PCNoDatabase (DummyAppState ref)
+       runSpock 5003 (spock spockCfg app)
+
+app :: SpockM () MySession MyAppState ()
+app =
+    do get root $
+           text "Hello World!"
